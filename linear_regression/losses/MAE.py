ff6bb2b0bb632aeec88b23bdac56bc3e15610a90
Maks0Ny <asandimaksim07@gmail.com>
2026-09-05 00:01:15 +0300

    Add MAE loss v2

--- linear_regression/losses/MAE.py
+++ linear_regression/losses/MAE.py
@@ -2,11 +2,13 @@ import numpy as np
 
 class MAE:
     
-    def forward(self, y_true, y_pred):
-        return np.mean(abs(y_true - y_pred))
+    def forward(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        
+        return np.mean(np.abs(y_true - y_pred))
     
-    def gradient(self, y_true, y_pred):
+    def gradient(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         n = len(y_true)
-        return ()
+        
+        return np.sign(y_pred, y_true)/n
         
     

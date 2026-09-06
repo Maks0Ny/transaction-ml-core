157a8282dbfdb5a99d390e3aeb18c87dc0780400
Maks0Ny <asandimaksim07@gmail.com>
2026-09-06 09:47:08 +0300

    Raw version of linear_regression

--- linear_regression/losses/MAE.py
+++ linear_regression/losses/MAE.py
@@ -9,4 +9,4 @@ class MAE:
     def gradient(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
         n = len(y_true)
         
-        return np.sign(y_pred, y_true)/n
+        return np.sign(y_pred, y_true) / n
--- linear_regression/models/linear_regression.py
+++ linear_regression/models/linear_regression.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class LinearRegression:
     
@@ -8,5 +8,25 @@ class LinearRegression:
         self.is_fitted = False
         
         
+    def initialize_weights(self, n_features: int) -> None:
+        self.weights = np.zeros(n_features)
+        self.bias = 0.0
+        
+        
+    def forward(self, X: np.ndarray) -> np.ndarray:
+        return np.dot(X, self.weights) + self.bias
     
     
+    def backward(self,  X: np.ndarray, grad_output: np.ndarray) -> tuple:
+        
+        dw = X.T @ grad_output
+        db = np.sum(grad_output)
+        
+        return dw, db        
+    
+    
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        if self.weights is None:
+            raise ValueError("Model is not fitted yet. Please call the 'fit' method first.")
+        
+        return self.forward(X)

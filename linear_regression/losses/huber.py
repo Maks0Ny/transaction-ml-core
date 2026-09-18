7a53aa1dbc2b9769e780110e9c423513fd82c413
Maks0Ny <asandimaksim07@gmail.com>
2026-09-18 16:32:09 +0300

    Add huber loss

--- linear_regression/losses/huber.py
+++ linear_regression/losses/huber.py
@@ -1,8 +1,27 @@
 import numpy as np
 
 class HuberLoss:
-    def __init__(self):
-        pass
+    
+    def forward(self, y_true, y_pred, delta=1.0) -> float:
+        
+        error = y_true - y_pred
+        abs_error = np.abs(error)
+    
+        quadratic_mask = abs_error <= delta
+        
+        quadratic_loss = 0.5 * (error ** 2)
+        linear_loss = delta * (abs_error - 0.5 * delta)
+        
+        return np.where(quadratic_mask, quadratic_loss, linear_loss)
     
     
-    
+    def gradient(self, y_true, y_pred, delta=1.0) -> np.ndarray:
+        
+        error = y_true - y_pred
+        abs_error = np.abs(error)
+        
+        quadratic_mask = abs_error <= delta
+        
+        gradient = np.where(quadratic_mask, -error, -delta * np.sign(error))
+        
+        return gradient / len(y_true)
